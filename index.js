4ec5e2cea24bdc045d5fcdcc2c608cf0fe692fc2
SamuliPunos <samuli.virtanen@punosmobile.com>
2021-03-29 12:36:30 +0300

    Changes to make module installable

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import {AppRegistry} from 'react-native';
+/* import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+ */
+
+import {LibraryCardModule} from './src/modules/LibraryCard';
+export {LibraryCardModule};
