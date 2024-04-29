7da1d4c6b1133b1a75ab3f2e7481cba15449a56f
aakelaukkanen <aake.laukkanen@punosmobile.com>
2024-04-29 09:40:35 +0300

    change library card screen to be in horizontal

--- src/modules/LibraryCard/components/LibraryCard.tsx
+++ src/modules/LibraryCard/components/LibraryCard.tsx
@@ -78,47 +78,48 @@ export const LibraryCard = ({cardNumber, holderName, logout, isFocused}: IProps)
       {text: locales.confirm.fi, onPress: () => logout()},
     ]);
   };
-
+  
   return (
     <View 
       style={styles.libraryCardContainer}
-      importantForAccessibility={isTimeout ? "no-hide-descendants" : "yes"}>    
-      <View style={styles.rotatedContainer}>
-        <View style={styles.libraryCard}>
-          <Text accessible={true} style={styles.holderName}>{holderName}</Text>
-          <View accessible={true} accessibilityLabel={locales.libraryBarCode.fi} accessibilityRole={'image'}>
-            <Barcode
-              text={cardNumber}
-              width={2.2}
-              height={90}
-              value={cardNumber}
-              format={'CODE39'}
-            />
+      importantForAccessibility={isTimeout ? "no-hide-descendants" : "yes"}
+    >             
+          <View style={styles.libraryCard}>
+            <Text style={styles.holderName}>{holderName}</Text>
+            <View accessibilityLabel={locales.libraryBarCode.fi} accessibilityRole={'image'}>
+              <Barcode
+                text={cardNumber}
+                width={1.65}
+                height={90}
+                value={cardNumber}
+                format={'CODE39'}
+              />
+            </View>
+            <Text style={styles.libraryCardDescription} accessible accessibilityRole={'text'}>
+              {locales.libraryCardDescription.fi}
+            </Text>
           </View>
-        </View>
-        <Text style={styles.libraryCardDescription} accessible accessibilityRole={'text'}>
-          {locales.libraryCardDescription.fi}
-        </Text>
-        <TouchableOpacity
-          accessible
-          accessibilityLabel={locales.pressLogout.fi}
-          style={styles.logoutButton}
-          accessibilityRole={'button'}
-          onPress={confirmLogout}
-          activeOpacity={0.6}>
-          <Text accessible={false} style={styles.buttonText}>
-            {locales.logoutButton.fi}
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.libraryCardImage}
-          accessibilityRole={'image'}
-          resizeMode={'contain'}
-          source={require('../../../assets/img/villirilli.png')}
-        />
-      </View>
+          <View style={styles.imageContainer}>
+              <Image
+                style={styles.libraryCardImage} 
+                accessibilityRole={'image'}
+                resizeMode={'contain'}
+                source={require('../../../assets/img/villirilli.png')}
+              />
+          </View>
+          <View style={styles.logoutContainer}>
+              <TouchableOpacity
+                accessible
+                accessibilityLabel={locales.pressLogout.fi}
+                style={styles.logoutButton}
+                accessibilityRole={'button'}
+                onPress={confirmLogout}
+                activeOpacity={0.6}>
+                <Text accessible={false} style={styles.buttonText}>
+                  {locales.logoutButton.fi}
+                </Text>
+              </TouchableOpacity>
+          </View>
     </View>
   );
 };
